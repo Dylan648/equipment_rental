5f46cf03f57644bc62903934ed6a0f4b18612ed5
Dylan648 <dbolter03@gmail.com>
2023-08-22 16:59:31 -0700

    initial commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageData } from "./$types"
+
+  export let data: PageData
+</script>
+
+<div class="h-screen flex items-center justify-center">
+  <h1>Login</h1>
+</div>
